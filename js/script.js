d2bcc60092210bfa9d519a3ccb8ad877f3d37e31
Leo9723 <pala9761@gmail.com>
2022-11-29 14:44:04 +0100

    inserimento elementi in html tramite js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
+let container = document.getElementById('container')
+
 for(let i = 1; i<=100; i++){
     let element = i
 
     if(i%3 == 0 && i%5 != 0){
-        element = 'Frizz'
+        element = `<div class="box box-green">Frizz</div>`;
     }
     else if(i%5 == 0 && i%3 != 0){
-        element = 'Buzz'
+        element = `<div class="box box-yellow">Buzz</div>`;
     }
     else if(i%5 == 0 && i%3 == 0){
-        element ='FrizzBuzz'
+        element = `<div class="box box-red">FrizzBuzz</div>`;
     }
     else{
-        element = i
+        element = `<div class="box box-blue">${i}</div>`;
     }
 
     console.log(element)
+
+    container.innerHTML += element
 }

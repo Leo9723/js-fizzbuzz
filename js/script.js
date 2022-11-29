50e47e60f40fc71526bce12f853ea92a792424a2
Leo9723 <pala9761@gmail.com>
2022-11-29 15:22:13 +0100

    aggiunta la possibilità di inserire il valore tramite area di testo e successivamente far sparire quest'ultima

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
 let container = document.getElementById('container')
 
-let number = prompt('inserisci un numero')
+const button = document.getElementById('button')
 
-for(let i = 1; i<=number; i++){
-    let element = i
+button.addEventListener('click', function(){
+    
+    let number = document.getElementById('number').value;
 
-    if(i%3 == 0 && i%5 != 0){
-        element = `<div class="box box-green">Frizz</div>`;
-    }
-    else if(i%5 == 0 && i%3 != 0){
-        element = `<div class="box box-yellow">Buzz</div>`;
-    }
-    else if(i%5 == 0 && i%3 == 0){
-        element = `<div class="box box-red">FrizzBuzz</div>`;
-    }
-    else{
-        element = `<div class="box box-blue">${i}</div>`;
+    var element = document.getElementById("form");
+    element.classList.add("none");
+    
+    for(let i = 1; i<=number; i++){
+        let element = i
+    
+        if(i%3 == 0 && i%5 != 0){
+            element = `<div class="box box-green">Frizz</div>`;
+        }
+        else if(i%5 == 0 && i%3 != 0){
+            element = `<div class="box box-yellow">Buzz</div>`;
+        }
+        else if(i%5 == 0 && i%3 == 0){
+            element = `<div class="box box-red">FrizzBuzz</div>`;
+        }
+        else{
+            element = `<div class="box box-blue">${i}</div>`;
+        }
+    
+        console.log(element)
+    
+        container.innerHTML += element
     }
 
-    console.log(element)
-
-    container.innerHTML += element
-}
+})
